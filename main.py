1613907944abcb61eb2f60b53448ea48788bbefe
PeterPan2151 <126306642+PeterPan2151@users.noreply.github.com>
2023-04-28 23:04:07 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+# user_prompt = 'Enter a To-Do: '
+#
+# todos = []
+# while True:
+#     todo = input(user_prompt)
+#     todos.append(todo)
+#     print(todos)
+
+# text = input("Enter a title: ")
+# length = len(text)
+# print(f"The book is {length} characters long.")
+
+# name = "Peter"
+# print(name)
+#
+# name_2 = input("Give me your name: ")
+# print(F"Hello {name_2}")
+#
+# names = ["peter", "frank", "Yasar", "preci"]
+
+# while True:
+#     print("Hello")
+
+# password = input("Enter password: ")
+# while password != "pass123":
+#     password = input("Enter password: ")
+# print("Password was correct")
+
+# num = 0
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+# name = input("Give me your name: ")
+# while True:
+#
+#     print(name.capitalize())
+#     name = input("Give me your name: ")
+
+# greeting = "Hello"
+# print(greeting.upper())
+
+countries = []
+while True:
+    country = input("Enter the country: ")
+    countries.append(country)
+    print(countries)
